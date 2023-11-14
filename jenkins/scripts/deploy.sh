3b38806b287858a71523bf43002e13b02aae9ddb
Sora1393 <yenyenn1433@gmail.com>
2023-11-14 21:26:23 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -x
 
 # Assuming C:\jenkins-php-selenium-test\src exists on your host machine
-docker run -d -p 80:80 --name my-apache-php-app -v C:\jenkins-php-selenium-test\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /c/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 
 set +x
